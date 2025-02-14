185dc55e7eb54190297fad66eb6088a904392c2c
Hiten <hitenmaru2000@gmail.com>
2025-02-14 15:04:31 +0530

    Made simple blast Enemy Game Logic

--- Assets/BlastEnemy/Script/Bullet.cs
+++ Assets/BlastEnemy/Script/Bullet.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float bulletSpeed;
+    public Vector2 direction;
 
     private void Update()
     {
-     
+        transform.Translate(direction * bulletSpeed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/BlastEnemy/Script/Enemy.cs
+++ Assets/BlastEnemy/Script/Enemy.cs
@@ -1,40 +1,63 @@
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     [SerializeField] private float enemySpeed;
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private GameObject firePos;
     [SerializeField] private GameObject bullet;
     [SerializeField] private float bulletSpeed;
 
+    private float storeEnemySpeed;
+
     private void Awake()
     {
         playerMovement = FindFirstObjectByType<PlayerMovement>();
+
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(ShootBullet), 1f, 2f);
+        storeEnemySpeed = enemySpeed;
+
     }
 
     private void Update()
     {
         MoveToPlayer();
-
-        if (Input.GetKeyDown(KeyCode.Space)) { ShootBullet(); }
-
     }
+
     private void MoveToPlayer()
     {
         Vector3 direction = (transform.position - playerMovement.transform.position).normalized;
         transform.Translate(-direction * enemySpeed * Time.deltaTime);
-       
- 
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+
+        float distance = Vector3.Distance(transform.position, playerMovement.transform.position);
+        {
+            if (distance <= 3f)
+            {
+                enemySpeed = 0;
+            }
+            else
+            {
+                enemySpeed = storeEnemySpeed;
+            }
+        }
     }
 
     private void ShootBullet()
     {
-        GameObject newSpwabObject = Instantiate(bullet,firePos.transform.position,Quaternion.LookRotation(firePos.transform.forward));
-        Rigidbody2D rigidbody2D = newSpwabObject.GetComponent<Rigidbody2D>();
-
-        rigidbody2D.linearVelocity = firePos.transform.forward * bulletSpeed * Time.deltaTime;
+        GameObject newSpwabObject = Instantiate(bullet, firePos.transform.position, Quaternion.identity);
+        Vector3 dir = (playerMovement.transform.position - firePos.transform.position).normalized;
+        Bullet bullets = newSpwabObject.GetComponent<Bullet>();
+        bullets.direction = dir;
 
     }
 
