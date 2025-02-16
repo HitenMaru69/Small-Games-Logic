e860f955a76e89315fa841a44fb30e74147c1b48
Hiten <hitenmaru2000@gmail.com>
2025-02-16 20:08:14 +0530

    Add some test code

--- Assets/Gold Minor/Script/RotateHook.cs
+++ Assets/Gold Minor/Script/RotateHook.cs
@@ -1,51 +1,104 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RotateHook : MonoBehaviour
 {
+    public bool isRotate;
+
     [SerializeField] GameObject rotateObject;
     [SerializeField] float rotateSpeed;
     [SerializeField] bool isReverceRotate;
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] GameObject ground;
+    
+
+    private Vector2 satrthookPosition = new Vector2(0f,5f);
 
     private void Start()
     {
-      
+        isRotate = true;
+
+
     }
 
     private void Update()
     {
+
+        MakeRope();
+
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+        
+            isRotate = false;
+
+        }
+
+        lineRenderer.SetPosition(0, ground.transform.position);
+        lineRenderer.SetPosition(1,rotateObject.transform.position);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotateObject.transform.Translate(Vector2.down * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if(rotateObject.transform.position.y <= 5)
+            {
+                rotateObject.transform.Translate(satrthookPosition * rotateSpeed * Time.deltaTime);
+            }
+
+        }
+
+        //if (Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    isRotate = true;
+        //}
+
         StartRotate();
     }
 
 
     private void StartRotate()
     {
-
-        float currentRotation = rotateObject.transform.eulerAngles.z;
-
-        if (isReverceRotate)
+        if (isRotate)
         {
-            rotateObject.transform.Rotate(new Vector3(0, 0, -40) * rotateSpeed * Time.deltaTime);
-        }
-        else 
-        {
-            rotateObject.transform.Rotate(new Vector3(0, 0, 40) * rotateSpeed * Time.deltaTime);
+
+            float currentRotation = rotateObject.transform.eulerAngles.z;
+
+            if (isReverceRotate)
+            {
+                rotateObject.transform.Rotate(new Vector3(0, 0, -40) * rotateSpeed * Time.deltaTime);
+            }
+            else
+            {
+                rotateObject.transform.Rotate(new Vector3(0, 0, 40) * rotateSpeed * Time.deltaTime);
+            }
+
+            if (currentRotation > 180)
+            {
+                currentRotation -= 360;
+            }
+
+
+            if (currentRotation >= 40f)
+            {
+                isReverceRotate = true;
+            }
+
+            if (currentRotation <= -40f)
+            {
+                isReverceRotate = false;
+            }
         }
 
-        if(currentRotation > 180)
-        {
-            currentRotation -= 360;
-        }
+    }
 
+    private void MakeRope()
+    {
+        lineRenderer.SetPosition(0,ground.transform.position);
+        lineRenderer.SetPosition(1,rotateObject.transform.position);
 
-        if (currentRotation >= 40f )
-        {
-            isReverceRotate = true;
-        }
-
-        if(currentRotation <= -40f)
-        {
-            isReverceRotate = false;
-        }
-
+        lineRenderer.startWidth = 0.5f;
+        lineRenderer.endWidth = 0.5f;
     }
 }
