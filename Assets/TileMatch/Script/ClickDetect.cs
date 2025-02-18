0c8c529b985bab42e5b79166ebe89063220575cb
Hiten <hitenmaru2000@gmail.com>
2025-02-18 16:21:21 +0530

    Working on match detect Logic

--- Assets/TileMatch/Script/ClickDetect.cs
+++ Assets/TileMatch/Script/ClickDetect.cs
@@ -5,17 +5,8 @@ public class ClickDetect : MonoBehaviour
 {
     public int number;
 
-    [SerializeField] Image image;
-    RectTransform rectTransforms;
-
-
-    private void Awake()
-    {
-        rectTransforms = GetComponent<RectTransform>();
-    }
-
     public void OnClickImage()
-    {
-
+    {    
+        Gamemanager.instance.AddObjectToTheList(gameObject);
     }
 }
--- Assets/TileMatch/Script/Gamemanager.cs
+++ Assets/TileMatch/Script/Gamemanager.cs
@@ -6,5 +6,36 @@ public class Gamemanager : MonoBehaviour
     public static Gamemanager instance;
 
     [SerializeField] private List<GameObject> totalBox;
+    [SerializeField] private List<GameObject> selectedObject = new();
     
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddObjectToTheList(GameObject obj)
+    {
+        selectedObject.Add(obj);
+        MoveObject();
+    }
+
+    public void MoveObject()
+    {
+        for (int i = 0; i < selectedObject.Count; i++)
+        {
+            RectTransform rectTransform1 = totalBox[i].GetComponent<RectTransform>();
+            RectTransform rectTransform2 = selectedObject[i].GetComponent<RectTransform>();
+
+            if (rectTransform2.anchoredPosition != rectTransform1.anchoredPosition)
+            {
+                rectTransform2.anchoredPosition = rectTransform1.anchoredPosition;
+            }
+        }
+    }
+
+    public void CheckForMatch()
+    {
+
+    }
 }
